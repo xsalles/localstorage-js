d3c4a479abe5acaa1881e29f97fe1b953dcc086c
xsalles <xs.salles@gmail.com>
2024-07-10 22:56:03 -0300

    refactored: styles css, and changed position of all itens. feat: created a form to remove items added to local storage.

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 let users = JSON.parse(localStorage.getItem('users')) || [];
 
 function save() {
-  localStorage.setItem("email", document.getElementById("email").value);
-  localStorage.setItem("password", document.getElementById("password").value);
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
   users.push({email, password})
 
@@ -16,19 +16,44 @@ function getData() {
 }
 
 function consult() {
-  const users = getData();
-  resultElement = document.getElementById('result')
+    const users = getData();
+    const resultElement = document.getElementById('result');
 
-  if(users.length > 0){
-    resultElement.innerHTML = 'Usuários: <br>'
-  
-  users.forEach(user => {
-    resultElement.innerHTML += `Email: ${user.email}, Senha: ${user.password} <br>`;
-  });
-} else {
-    resultElement.innerText = 'Nenhum usuário foi encontrado'
+    if (users.length > 0) {
+        resultElement.innerHTML = '';
+      
+            users.forEach((user, index) => {
+                const userDiv = document.createElement('div');
+                userDiv.classList.add('user-item');
+
+                const data = document.createElement('p');
+                data.textContent = `Email: ${user.email} Senha: ${user.password}`
+
+                const remove = document.createElement('button')
+                remove.textContent = 'Remover'
+                remove.addEventListener('click', () => deleteUser(index))
+
+                userDiv.appendChild(data, remove)
+                userDiv.appendChild(remove)
+                resultElement.appendChild(userDiv)
+            });
+    } else {
+        resultElement.innerText = 'Nenhum usuário foi encontrado';
+    }
+} 
+
+function deleteUser(index){
+    let users = getData();
+
+    if(users.length > index){
+        users.splice(index, 1);
+        localStorage.setItem('users', JSON.stringify(users));
+        alert('Usuário removido com sucesso')
+        consult()
+    } else {
+        alert('Índice invalido')
+    }
 }
-};
 
 
 function modify() {
@@ -56,6 +81,7 @@ function showModifySection(){
     document.getElementById('modifysection').style.display = 'flex';
     document.getElementById('modifysection').style.flexDirection = 'column';
     document.getElementById('modifysection').style.gap = '10px';
+    document.getElementById('modifysection').style.color = 'azure';
 }
 
 document.getElementById("loginForm").addEventListener("submit", function (event) {
